45898c09e4f9055845b1b0fcfc31cadf2df90f0e
fahmi54321 <fahmiabdulaziz44@gmail.com>
2021-05-01 11:20:20 +0700

    sliding images

--- app/src/main/java/com/android/a3slidingimagewithviewpager/MainActivity.java
+++ app/src/main/java/com/android/a3slidingimagewithviewpager/MainActivity.java
@@ -1,14 +1,27 @@
 package com.android.a3slidingimagewithviewpager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    ViewPager viewPager;
+    ViewPagerAdapter viewPagerAdapter;
+    private String[] images = {
+            "https://www.google.co.id/imgres?imgurl=https%3A%2F%2Fp4.wallpaperbetter.com%2Fwallpaper%2F135%2F708%2F936%2Fbeautiful-pictures-of-nature-1920x1200-wallpaper-preview.jpg&imgrefurl=https%3A%2F%2Fwww.wallpaperbetter.com%2Fid%2Fhd-wallpaper-umfum&tbnid=xCp3HPPn-cu4GM&vet=12ahUKEwintLqxy6fwAhVx0nMBHTfODEYQMygEegQIARBd..i&docid=tYMUYy6woTYvZM&w=728&h=455&itg=1&q=gambarindah&hl=en&safe=strict&ved=2ahUKEwintLqxy6fwAhVx0nMBHTfODEYQMygEegQIARBd",
+            "https://www.google.co.id/imgres?imgurl=https%3A%2F%2Flh6.googleusercontent.com%2Fproxy%2Fa4o3sG9Mgv8wNrrvbRc4V4m_ZFWeDro4RYeCprZ4I7RskC0wdmzQTd2nGea8Yw-evvF0fp9D5VJ9gy2CM8AIVe9KY1K0w1p73IKiOcHRx5FnAmMTPy5eMX0yfRfXF0qa1zGpwJ5je6aNClQ%3Ds0-d&imgrefurl=https%3A%2F%2Fvinagambar.blogspot.com%2F2021%2F02%2Fcari-gambar-indah-kota-jogja.html&tbnid=BNS4w0HAxsA6FM&vet=12ahUKEwintLqxy6fwAhVx0nMBHTfODEYQMygPegQIARB1..i&docid=VxfXS-jM5zOOoM&w=900&h=644&itg=1&q=gambarindah&hl=en&safe=strict&ved=2ahUKEwintLqxy6fwAhVx0nMBHTfODEYQMygPegQIARB1"
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        viewPagerAdapter = new ViewPagerAdapter(MainActivity.this,images);
+        viewPager.setAdapter(viewPagerAdapter);
+
     }
 }
---
+++ app/src/main/java/com/android/a3slidingimagewithviewpager/ViewPagerAdapter.java
@@ -0,0 +1,74 @@
+package com.android.a3slidingimagewithviewpager;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.bumptech.glide.Glide;
+import com.squareup.picasso.Picasso;
+
+public class ViewPagerAdapter extends PagerAdapter {
+
+    Activity activity;
+    String[] images;
+    LayoutInflater inflater;
+
+    public ViewPagerAdapter(Activity activity, String[] images) {
+        this.activity = activity;
+        this.images = images;
+    }
+
+    @Override
+    public int getCount() {
+        return images.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view==object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        inflater = (LayoutInflater)activity.getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View itemView = inflater.inflate(R.layout.viewpager_item,container,false);
+
+        ImageView imageView;
+        imageView = (ImageView)itemView.findViewById(R.id.image);
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        activity.getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int height = displayMetrics.heightPixels;
+        int width = displayMetrics.widthPixels;
+        imageView.setMinimumHeight(height);
+        imageView.setMinimumWidth(width);
+
+        try {
+            Glide.with(activity.getApplicationContext())
+                    .load(images[position])
+                    .placeholder(R.mipmap.ic_launcher)
+                    .error(R.mipmap.ic_launcher_round)
+                    .into(imageView);
+        }catch (Exception ex){
+            Log.e(ViewPagerAdapter.class.getSimpleName(),ex.getMessage());
+        }
+
+        container.addView(itemView);
+        return itemView;
+
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        ((ViewPager) container).removeView((View)object);
+    }
+}
